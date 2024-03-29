4c823a4bb0d6d298a2f090584848655743116c5c
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-29 18:08:39 +0530

    d:\Basic in C++\Basic-C-\Areaofcircle.cc

---
+++ Areaofcircle.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Calculating Area of circle Using Variables  A=piR^2   float r = 4.3
+    float r = 4.3;
+    float a = 3.141592*r*r;
+    cout<<"Area is :"<<endl;
+    cout<<a<<endl;
+    
+
+}
