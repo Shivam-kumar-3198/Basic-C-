6522782c19a1ba04a433d7fd774d654b447b5b25
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-29 18:09:25 +0530

    d:\Basic in C++\Basic-C-\increment&decrement operators.cc

---
+++ increment&decrement operators.cc
@@ -0,0 +1,30 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    cout<<x<<endl;
+    cout<<x++<<endl;
+    cout<<x<<endl;
+}
+*/
+/*
+output
+4
+4
+5
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    cout<<x<<endl;
+    cout<<++x<<endl;
+    cout<<x<<endl;
+}
+/*Output
+4
+5
+5
+*/
