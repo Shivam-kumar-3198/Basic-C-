d52e8ee3dee9a9e4ce4d4bfd382b4a2f81916ce8
Shivam <thisisshivamkumar8840@gmail.com>
2024-03-30 19:43:20 +0530

    d:\Basic in C++\Basic-C-\sum.cc

---
+++ sum.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int x;
+      cout<<"Enter a number X =";
+      cin>>x;
+     int y;
+     cout<<"Enter a number Y =";
+     cin>>y;
+
+  cout<<"Sum is "<<x+y;
+
+     }
