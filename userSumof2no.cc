7b71f3f428401c22d7d94a0c6d8ba6c7c6c9ec2c
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-03 10:53:02 +0530

    d:\Basic in C++\Basic-C-\userSumof2no.cc

---
+++ userSumof2no.cc
@@ -0,0 +1,28 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number x:";
+    cin>>x;
+    int y;
+    cout<<"Enter a number y:";
+    cin>>y;
+    cout<<x+y;
+} 
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter first number of your choice :";
+    cin>>x;
+    int y;
+    cout<<"Enter second number of your choice :";
+    cin>>y;
+
+    int c;
+    cout<<x+y;
+    
+
+}
