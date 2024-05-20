9d4feb2a41b3cb49e9d630cde42adc37a1118208
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-20 18:21:36 +0530

    d:\Basic in C++\Basic-C-\sum2.cc

---
+++ sum2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter first Number: ";
+    cin >> x;
+
+    int y;
+    cout << "Enter second Number: ";
+    cin >> y;
+
+    cout << "Sum: " << x + y << endl;
+
+    return 0;
+}
