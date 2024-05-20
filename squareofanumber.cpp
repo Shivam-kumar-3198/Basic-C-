4fe99eec581c313511650d2115a435cd5008db70
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-20 17:55:17 +0530

    d:\Basic in C++\Basic-C-\squareofanumber.cpp

---
+++ squareofanumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x =4;
+    cout<<"Enter a number you want to make square :";
+    cin>>x;
+    cout<<"Square of the number is:";
+    cout<<x*x;
+}
