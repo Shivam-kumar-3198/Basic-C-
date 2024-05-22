f288c8b4e23a73ed13eb37a7fd183fe39aa04637
Shivam <thisisshivamkumar8840@gmail.com>
2024-05-22 18:33:16 +0530

    d:\Basic in C++\Basic-C-\Modulus Operator

---
+++ Modulus Operator/modulus.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the term you want to make triple:";
+    cin>>x;
+    int y;
+    cout<<"Triple of the number is:";
+    cout<< x*x*x;
+
+}
