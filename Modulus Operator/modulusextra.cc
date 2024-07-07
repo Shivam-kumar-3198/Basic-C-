23848db3a70f26bdbc1daa4b4dd2e5efe2048282
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-07 08:05:11 +0530

    d:\Basic in C++\Basic-C-\Modulus Operator

---
+++ Modulus Operator/modulusextra.cc
@@ -0,0 +1,18 @@
+// a % b = a [if a<b]
+
+// a % a  = 0
+
+// a % (-b) = a % b itand
+
+//(-a) % b = [a % b] 
+
+// number 1
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 2;
+    int y = 8;
+    cout<<x%y;
+}
+
+//Importance of modulus = importance is it's divisibility 
