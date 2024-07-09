1feccbe1d1ceaf03127447c9cad53f284773abac
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-09 08:01:40 +0530

    d:\Basic in C++\Basic-C-\Ascii_values

---
+++ Ascii_values/allScii.cc
@@ -0,0 +1,144 @@
+#include <iostream>
+
+int main() {
+    // Print ASCII codes from 0 to 127
+    for (int i = 0; i < 128; ++i) {
+        std::cout << "Character: '" << char(i) << "', ASCII code: " << i << std::endl;
+    }
+
+    return 0;
+}
+
+//Output
+
+// Character: '', ASCII code: 0
+// Character: '☺', ASCII code: 1
+// Character: '☻', ASCII code: 2
+// Character: '♥', ASCII code: 3
+// Character: '♦', ASCII code: 4
+// Character: '♣', ASCII code: 5
+// Character: '♠', ASCII code: 6
+// Character: '', ASCII code: 7
+// Character: ', ASCII code: 8
+// Character: '    ', ASCII code: 9
+// Character: '
+// ', ASCII code: 10
+// Character: '
+// ', ASCII code: 11
+// Character: '
+// ', ASCII code: 12
+// ', ASCII code: 13
+// Character: '', ASCII code: 14
+// Character: '', ASCII code: 15
+// Character: '►', ASCII code: 16
+// Character: '◄', ASCII code: 17
+// Character: '↕', ASCII code: 18
+// Character: '‼', ASCII code: 19
+// Character: '¶', ASCII code: 20
+// Character: '§', ASCII code: 21
+// Character: '▬', ASCII code: 22
+// Character: '↨', ASCII code: 23
+// Character: '↑', ASCII code: 24
+// Character: '↓', ASCII code: 25
+// Character: '→', ASCII code: 26
+// Character: 'SCII code: 27
+// Character: '∟', ASCII code: 28
+// Character: '↔', ASCII code: 29
+// Character: '▲', ASCII code: 30
+// Character: '▼', ASCII code: 31
+// Character: ' ', ASCII code: 32
+// Character: '!', ASCII code: 33
+// Character: '"', ASCII code: 34
+// Character: '#', ASCII code: 35
+// Character: '$', ASCII code: 36
+// Character: '%', ASCII code: 37
+// Character: '&', ASCII code: 38
+// Character: ''', ASCII code: 39
+// Character: '(', ASCII code: 40
+// Character: ')', ASCII code: 41
+// Character: '*', ASCII code: 42
+// Character: '+', ASCII code: 43
+// Character: ',', ASCII code: 44
+// Character: '-', ASCII code: 45
+// Character: '.', ASCII code: 46
+// Character: '/', ASCII code: 47
+// Character: '0', ASCII code: 48
+// Character: '1', ASCII code: 49
+// Character: '2', ASCII code: 50
+// Character: '3', ASCII code: 51
+// Character: '4', ASCII code: 52
+// Character: '5', ASCII code: 53
+// Character: '6', ASCII code: 54
+// Character: '7', ASCII code: 55
+// Character: '8', ASCII code: 56
+// Character: '9', ASCII code: 57
+// Character: ':', ASCII code: 58
+// Character: ';', ASCII code: 59
+// Character: '<', ASCII code: 60
+// Character: '=', ASCII code: 61
+// Character: '>', ASCII code: 62
+// Character: '?', ASCII code: 63
+// Character: '@', ASCII code: 64
+// Character: 'A', ASCII code: 65
+// Character: 'B', ASCII code: 66
+// Character: 'C', ASCII code: 67
+// Character: 'D', ASCII code: 68
+// Character: 'E', ASCII code: 69
+// Character: 'F', ASCII code: 70
+// Character: 'G', ASCII code: 71
+// Character: 'H', ASCII code: 72
+// Character: 'I', ASCII code: 73
+// Character: 'J', ASCII code: 74
+// Character: 'K', ASCII code: 75
+// Character: 'L', ASCII code: 76
+// Character: 'M', ASCII code: 77
+// Character: 'N', ASCII code: 78
+// Character: 'O', ASCII code: 79
+// Character: 'P', ASCII code: 80
+// Character: 'Q', ASCII code: 81
+// Character: 'R', ASCII code: 82
+// Character: 'S', ASCII code: 83
+// Character: 'T', ASCII code: 84
+// Character: 'U', ASCII code: 85
+// Character: 'V', ASCII code: 86
+// Character: 'W', ASCII code: 87
+// Character: 'X', ASCII code: 88
+// Character: 'Y', ASCII code: 89
+// Character: 'Z', ASCII code: 90
+// Character: '[', ASCII code: 91
+// Character: '\', ASCII code: 92
+// Character: ']', ASCII code: 93
+// Character: '^', ASCII code: 94
+// Character: '_', ASCII code: 95
+// Character: '`', ASCII code: 96
+// Character: 'a', ASCII code: 97
+// Character: 'b', ASCII code: 98
+// Character: 'c', ASCII code: 99
+// Character: 'd', ASCII code: 100
+// Character: 'e', ASCII code: 101
+// Character: 'f', ASCII code: 102
+// Character: 'g', ASCII code: 103
+// Character: 'h', ASCII code: 104
+// Character: 'i', ASCII code: 105
+// Character: 'j', ASCII code: 106
+// Character: 'k', ASCII code: 107
+// Character: 'l', ASCII code: 108
+// Character: 'm', ASCII code: 109
+// Character: 'n', ASCII code: 110
+// Character: 'o', ASCII code: 111
+// Character: 'p', ASCII code: 112
+// Character: 'q', ASCII code: 113
+// Character: 'r', ASCII code: 114
+// Character: 's', ASCII code: 115
+// Character: 't', ASCII code: 116
+// Character: 'u', ASCII code: 117
+// Character: 'v', ASCII code: 118
+// Character: 'w', ASCII code: 119
+// Character: 'x', ASCII code: 120
+// Character: 'y', ASCII code: 121
+// Character: 'z', ASCII code: 122
+// Character: '{', ASCII code: 123
+// Character: '|', ASCII code: 124
+// Character: '}', ASCII code: 125
+// Character: '~', ASCII code: 126
+// Character: '', ASCII code: 127
---
+++ Ascii_values/base_ascii.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    cout<<(int)ch;
+}
+
+
+// Uppercase letters (A to Z):
+// A: ASCII value = 65
+// B: ASCII value = 66
+// …
+// Z: ASCII value = 90
+
+
+// Lowercase letters (a to z):
+// a: ASCII value = 97
+// b: ASCII value = 98
+// …
+// z: ASCII value = 122
