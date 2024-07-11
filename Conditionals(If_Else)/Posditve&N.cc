4b4fb1ec7dc32b4ba5a0e1955cef4d9b81db91ac
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-11 07:57:56 +0530

    d:\Basic in C++\Basic-C-\Conditionals(If_Else)\Posditve&N.cc

---
+++ Conditionals(If_Else)/Posditve&N.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter N:";
+    cin>>n;
+    if(n%2==0); //even hai
+    cout<<"Even Number";
+    if(n%2!=0);
+    cout<<"Odd Number";
+
+
+}
