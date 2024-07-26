5f89e69a27f2a1352e441ba9e5922d3f9b931923
Shivam <thisisshivamkumar8840@gmail.com>
2024-07-26 07:14:44 +0530

    d:\Basic in C++\Basic-C-\LOOPs\whileLoop\printfactorial.cc

---
+++ LOOPs/whileLoop/printfirstnfactorial.cc
@@ -0,0 +1,8 @@
+// Write a program to print the ASCII Value
+#include<iostream>
+using namespace std;
+int main(){
+    for(char ch = 'a'; ch<='z'; ch++){
+     cout<<"Ascii code of "<<ch<<"="<<int(ch)<<endl;
+    }
+}
