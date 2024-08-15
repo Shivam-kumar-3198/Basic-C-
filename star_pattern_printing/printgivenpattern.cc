a7b26dcbf1ae91b7838c296583acd4fbbc605fc5
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-15 08:23:00 +0530

    .

--- star_pattern_printing/printgivenpattern.cc
+++ star_pattern_printing/printgivenpattern.cc
@@ -8,7 +8,7 @@ int main(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             if(i==mid || j==mid) cout<<"* ";
-            else cout<<"  ";
+            else cout<<"# ";
         }
         cout<<endl;
     }
