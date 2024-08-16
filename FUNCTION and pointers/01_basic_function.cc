f0d07849a50520b7e4650c9f4a570bba5c3e947f
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-16 08:15:14 +0530

    .

---
+++ FUNCTION and pointers/01_basic_function.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+    void usa(){
+        cout<<"You are in USA"<<endl;
+    }
+    void india(){
+        cout<<"You are in Indian Server"<<endl;
+    }
+
+    int main(){
+        cout<<"Your country is : "<<endl;
+        india();
+        usa();
+        
+    }
