9d0da4bee3ec126f7e7a650acad9ad4e7f7965ea
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-17 07:57:08 +0530

    .

---
+++ FUNCTION and pointers/return.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void usa(){
+    cout<<"You are in USA"<<endl;
+    return;   //If you return your statement then program will stop there
+    cout<<"You are underworld"<<endl;
+    
+}
+
+int main(){
+    usa();
+}
