aa928c27d5a81629841856cb4420bd2649c3360a
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-19 07:39:40 +0530

    .

---
+++ FUNCTION and pointers/multipleFunction.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void sum(int a, int b){
+    cout<<a+b;
+}
+
+int main(){
+    int a;
+    cout<<"Enter first Number";
+    cin>>a;
+    
+    int b;
+    cout<<"Enter first Number";
+    cin>>b;
+    sum(a,b);
+
+}
