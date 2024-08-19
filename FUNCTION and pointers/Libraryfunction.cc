f8b213d74c65cfb8c55688f0a520148d48243a68
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-19 08:03:17 +0530

    .

---
+++ FUNCTION and pointers/Libraryfunction.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath> // Corrected header file
+
+using namespace std;
+
+int main() {
+    cout << sqrt(4);
+    return 0; // Add a return statement to indicate successful execution
+}
--- FUNCTION and pointers/return_type_sum.cc
+++ FUNCTION and pointers/return_type_sum.cc
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sum(int a, int b){
-    return a+b;
+int sum(int a, int b) {
+    return a + b;
 }
 
-int main(){
-    cout<<sum(7,90);
-    
+int main() {
+    cout << sum(7, 90);
+    return 0; // Don't forget to include a return statement in the main function
 }
