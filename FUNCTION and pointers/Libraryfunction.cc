946bed79df35175150a86a471dc38682879ebb2f
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-20 07:55:48 +0530

    .

--- FUNCTION and pointers/Libraryfunction.cc
+++ FUNCTION and pointers/Libraryfunction.cc
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main() {
-    cout << cbrt(9);
-    return 0; // Add a return statement to indicate successful execution
+    // cout << cbrt(9);
+    // return 0; // Add a return statement to indicate successful execution
+     cout<<max(4,8)<<endl;
+     cout<<min(4,8);
 }
---
+++ FUNCTION and pointers/permutation&combination.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+
+    int r;
+    cout<<"Enter r :";
+    cin>>r;
+
+    int a = 1;
+    for(int i=1; i<=n;i++){
+        a+=1;
+    }
+
+    int b = 1;
+    for(int i =1; i<=r; i++){
+        b *= i;
+    }
+
+    int c = 1;
+    for(int i =1; i<=n-r;i++){
+        c *= i;
+    }
+
+    cout<<a/(b*c);
+
+}
