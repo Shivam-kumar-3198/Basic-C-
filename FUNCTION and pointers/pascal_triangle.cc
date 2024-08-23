3c2ef2fd7e1d32be4ba7396a0ab037f79bd386d9
Shivam <thisisshivamkumar8840@gmail.com>
2024-08-23 07:35:46 +0530

    .

--- FUNCTION and pointers/pascal_triangle.cc
+++ FUNCTION and pointers/pascal_triangle.cc
@@ -1,12 +1,12 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Enter n : ";
-    cin>>n;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Enter n : ";
+//     cin>>n;
 
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=i;j++){
-            cout<<ncr(i,j)
-        }
-    }
-}
+//     for(int i=0;i<=n;i++){
+//         for(int j=0;j<=i;j++){
+//             cout<<ncr(i,j)
+//         }
+//     }
+// }
