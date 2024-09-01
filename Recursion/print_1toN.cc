06483b3a163a6d1abe32b188215faa07220c0c62
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-01 07:50:39 +0530

    .

--- Recursion/print_1toN.cc
+++ Recursion/print_1toN.cc
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+
+void print(int n){
+    if(n==11) return;
+    cout<<n<<endl;
+    print(n+1);
+}
 int main(){
-    
+    // int n;
+    // cout<<"Enter n :";
+    // cin>>n;
+    print(1);
 }
