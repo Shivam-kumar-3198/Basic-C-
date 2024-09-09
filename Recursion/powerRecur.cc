9df21990f8007cd1c82824acb7ca5138d033c1c1
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-09 07:53:43 +0530

    .

--- Recursion/powerRecur.cc
+++ Recursion/powerRecur.cc
@@ -5,4 +5,12 @@ int main(){
         if(n==0 || n==1) return 1;
         return n*fact(n-1);  
 }
-
+int main(){
+    int a;
+    cout<<"Enter base : ";
+    cin>>a;
+    int b;
+    cout<<"Enter power : ";
+    cin>>b;
+    cout<<pow(a,b);
+}
