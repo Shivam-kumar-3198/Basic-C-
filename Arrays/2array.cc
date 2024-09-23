affa48c23a58d995327623986585e895e22c9bd5
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-23 08:25:19 +0530

    .

---
+++ Arrays/2array.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    arr[0] = 6;
+    arr[1] = 2;
+    arr[2] = 45;
+    arr[3] = 64;
+    arr[4] = 74;
+    arr[5] = 90;
+    for(int i=0;i<=4;i++){
+        cout<<arr[i]<<" ";
+    }
+    // We have solved the array problem using Loop
+    // In the next problem we have to take input form the user
+
+}
---
+++ Arrays/3array.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    for(int i=0;i<=3;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<=3;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+// we have to print the array from right to left
