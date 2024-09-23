45e02c4215593e4adc78f25e378b897330a0b548
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-23 08:05:39 +0530

    .

--- Arrays/1st.cc
+++ Arrays/1st.cc
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x[5];
-    cout<<"Enter the array : ";
-    cin>>x;
+    int arr[5];
+    arr[0] = 6;
+    arr[1] = 2;
+    arr[2] = 8;
+    arr[3] = 5;
+    arr[4] = 0;
+    cout<<arr[0]<<" ";
+    cout<<arr[1]<<" ";
+    cout<<arr[2]<<" ";
+    cout<<arr[3]<<" ";
+    cout<<arr[4]<<" ";
+    // This is manual way to print an array we have to use loop for this
 }
