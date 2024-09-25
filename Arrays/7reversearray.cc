d4c2b6f57d1697fe75c13139ae16062da94c5544
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-25 08:21:14 +0530

    .

--- Arrays/7reversearray.cc
+++ Arrays/7reversearray.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {9,8,7,6,2};
+    int arr[5] = {9,8,7,6,2};//no need to declare array value agar value already daal diya hai to
     // cout<<"Enter the numbers you want to print : ";
     cout<<"Registered numbers are"<<endl;
     for(int i=4;i>=0;i--){
