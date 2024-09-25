68c136cd69697914c13f81a6cbb9c379e4cb9c2c
Shivam <thisisshivamkumar8840@gmail.com>
2024-09-25 08:03:35 +0530

    .

---
+++ Arrays/7reversearray.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {9,8,7,6,2};
+    // cout<<"Enter the numbers you want to print : ";
+    cout<<"Registered numbers are"<<endl;
+    for(int i=4;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+
+}
