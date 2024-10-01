baf12c4bbe33fe8f7ca42277ee74eddb0b75e37c
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-01 08:03:21 +0530

    .

--- Arrays/Homeworks/questtion7.cc
+++ Arrays/Homeworks/questtion7.cc
@@ -1,5 +1,9 @@
+// findd the minimum value of all the elements the array
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<INT64_MIN;
+    // cout<<INT64_MIN;
+    int num[5];
+    num[5] = 11;
+    cout<<num[5];
 }
