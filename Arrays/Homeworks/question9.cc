708812ac5d8fd11e99e2805b7e66a1e7f1633889
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-04 08:09:48 +0530

    .

--- Arrays/Homeworks/question9.cc
+++ Arrays/Homeworks/question9.cc
@@ -1,1 +1,12 @@
 // Count the number of elements in given array greater than a given number x
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,3,0,10,2,5,6};
+    int x = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    int count = count_if(arr, arr + n, [x](int num{}))
+
+
+}
