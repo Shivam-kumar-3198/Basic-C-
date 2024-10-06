30fab99ea01f808f9bcc3b765498835c86ca2c77
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-06 08:29:05 +0530

    .

--- Arrays/15reversearray.cc
+++ Arrays/15reversearray.cc
@@ -4,8 +4,23 @@ using namespace std;
 int main(){
       int a[] = {1,2,3,4,5,6,7,8};
       int n = sizeof(a)/4;
+      int i = 0;
+      int j = n-1;
+
       for(int i=0; i<n; i++){
-        a[i] = a[n-1-i];
+        cout<<a[i]<<" ";
+      }
+
+      while(i<j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+      }
+
+      cout<<endl;
+      for(int i=0; i<n; i++){
         cout<<a[i]<<" ";
       }
 }
