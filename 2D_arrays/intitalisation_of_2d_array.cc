52f898b06223065f9c8e13e7c0abf813f7bf1975
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-11 08:10:38 +0530

    .

---
+++ 2D_arrays/intitalisation_of_2d_array.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+    int arr[4][2] = {{1234,56}, {1256, 43}, {1434,32}, {1312,96}};
+    int arr[4][2] = {1234,56,1256,43,1434,32,1312,96};
+    int arr[2][3] = {12, 34,56,78,98,23};
+    int arr [][3] = {1,2,3 ,4,5,6,7,8,9}  agar hum saath saath declare kar rhe hai tbhi hi aise array declare karenge
+    
+    */
+}
