4c2b4813d2fbcd7b9da9294608ba0f139d95fb46
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-12 08:09:57 +0530

    .

--- 2D_arrays/question2.cc
+++ 2D_arrays/question2.cc
@@ -1,1 +1,22 @@
 // Write a program to store 10 at every index of a 2D matrix with 5 rows and 5 colums
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[5][5];
+    
+    for (int i=0; i<5; i++){ // this is used for loop over rows
+        for(int j=0; j<5; j++){  // this is used for  loop over columns
+            arr[i][j] = 10; //here we added the value of array (declared)
+        }
+    }
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cout<<arr[i][j] <<" ";
+        }
+         cout<<endl;
+    }
+   
+    return 0;
+}
