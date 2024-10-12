afe20756fa36945b41e3c394c276bad928aa0829
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-12 07:38:48 +0530

    .

--- 2D_arrays/question1.cc
+++ 2D_arrays/question1.cc
@@ -3,11 +3,20 @@
 using namespace std;
 int main(){
     int arr[4][2] = {{76,81},{13,76},{82,91},{88,90}};
-    
+   // Calculate the number of rows
+    int m = sizeof(arr) / sizeof(arr[0]);
+
+    // Calculate the number of columns (optional, just for demonstration)
+    int n = sizeof(arr[0]) / sizeof(arr[0][0]);
+
+
     cout<<"Roll Number and marks of students :" << endl;
-    for(int i = 0; i<4;i++){
-        cout<<"roll Number :" << arr[i][0] << ", Marks" <<arr[i][i] << endl;
+    for(int i = 0; i<m;i++){
+        // for(int j=0; j<n; j++){
+            cout << "Roll Number: " << arr[i][0] << ", Marks: " << arr[i][1] << endl;
+        // }
     }
+
 
     return 0;
 
