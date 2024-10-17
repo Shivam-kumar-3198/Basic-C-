c6380b2f46a417a380801e56f23665a78f833d72
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-17 08:22:04 +0530

    .

---
+++ Strings/indexinginstrings.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "shivam Singh";
+    cout<<s<<endl;
+    s[0] = 'M';
+    s[2] = 'u';
+    s[3] = 'b';
+    cout<<s<<endl;
+
+}
