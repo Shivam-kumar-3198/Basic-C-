029e10780e45ae211d69d31067e60cf8ee597296
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-18 07:47:53 +0530

    .

---
+++ Strings/queston2.cc
@@ -0,0 +1,20 @@
+//use of push back
+#include<iostream>
+using namespace std;
+int main(){
+      string s = "Shivam";
+      cout<<s.length()<<endl;
+      
+    //   s.push_back('j');
+    //   cout<<s<<endl;
+    //   s.push_back('i');
+    //   cout<<s<<endl;
+
+    // s.pop_back();
+    // cout<<s<<endl;  //ek letter hat jayega
+
+    s.append(" kumar");
+    cout<<s.length()<<endl;
+
+
+}
