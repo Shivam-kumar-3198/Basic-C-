34744c7dea94013047dff1ee958723d6c25e0410
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-18 07:31:30 +0530

    .

--- Strings/question1.cc
+++ Strings/question1.cc
@@ -1,10 +1,23 @@
 // Input a string of length n and count all the vowels in the given string
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string s = "Shivam is the best coder";
+//     int n = s.length();
+//     for(int i=0; i<n; i++){
+//         cout<<s[i];
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    string s = "Shivam is the best coder";
-    int n = s.length();
-    for(int i=0; i<n; i++){
-        cout<<s[i];
+    string s = "Cow is the domestic animal";
+    int count = 0;
+    for(int i=0;i<s.length(); i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+            count++;
+        }
     }
+    cout<<count;
 }
