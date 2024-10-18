90c7356e54b1d25ed2060607ccd3736477d1dc4d
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-18 07:20:47 +0530

    .

---
+++ Strings/question1.cc
@@ -0,0 +1,10 @@
+// Input a string of length n and count all the vowels in the given string
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "Shivam is the best coder";
+    int n = s.length();
+    for(int i=0; i<n; i++){
+        cout<<s[i];
+    }
+}
