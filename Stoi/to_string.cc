e14a18b479d9846f1a691387bb46305ccf20a01e
Shivam <thisisshivamkumar8840@gmail.com>
2024-10-30 17:41:08 +0530

    .

---
+++ Stoi/to_string.cc
@@ -0,0 +1,12 @@
+// intger to string converter
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4534563;
+    string s = to_string(x);
+    s =  s + "dd";
+    cout<<s;
+
+}
+
+// Completed the C++ course
