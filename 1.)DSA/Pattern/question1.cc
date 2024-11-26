7ea55fbc5d76a4cf151367602d44b54f2a68c30a
Shivam <thisisshivamkumar8840@gmail.com>
2024-11-26 17:17:34 +0530

    .

--- 1.)DSA/Pattern/question1.cc
+++ 1.)DSA/Pattern/question1.cc
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int a;
+    cout<<"Enter the number";
+    cin>>a;
+
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=a; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    cout<<a;
 }
