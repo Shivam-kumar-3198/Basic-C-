5606d6db96ddc7ea615bbbe061c4255a411377b6
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-05 23:49:44 +0530

    .

---
+++ 1.)DSA/Operators_and_forLoop/1stquestion.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 6;
+
+    cout<<"A & B ="<< (a&b)<<endl; //And Operator
+    cout<<"A | B ="<< (a|b)<<endl; //Or Operator
+    cout<<"A ^ B ="<< (a^b)<<endl; //XOR Operator
+    cout<<"A ~ B ="<< ~a<<endl;    // Nor Operator
+
+}
