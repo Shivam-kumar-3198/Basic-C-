44bed5440dfa62643bf8e708d49b0f846edb8b95
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-09 19:38:57 +0530

    .

--- 1.)DSA/Operators_and_forLoop/forloop.cc
+++ 1.)DSA/Operators_and_forLoop/forloop.cc
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,5 +11,22 @@ int main(){
 
     for(int i=1; i<=n; i++ ){
         cout<<i<<endl;
+    }
+}
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    // int n;
+    // cout<<"Enter the value of n" <<endl;
+    // cin>>n;
+
+    // cout<<"Printing the value from 1 to n " <<endl;
+
+    for(int a=0, b=1, c=2; a>=0 && b>=1 &&c>=2; a--,b--,c--){
+        cout<<a<<" "<<b <<" "<<c<<endl;
     }
 }
