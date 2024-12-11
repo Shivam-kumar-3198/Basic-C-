e2a8147f8d01766ba15edd7436f92f6e0ab3d93a
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-11 09:58:33 +0530

    .

---
+++ 1.)DSA/Operators_and_forLoop/practiceQ.cc
@@ -0,0 +1,30 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i<=5; i++){
+        cout<<i<<" ";
+        i++;
+    }
+}
+*/
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i<=5; i--){
+        cout<<i<<" ";
+        i++;
+    }
+}
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            cout<<i << " " << j <<endl;
+        }
+    }
+}
