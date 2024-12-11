5fb9a8d8530890ce43b567b0b14d64621bfafde5
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-11 09:45:09 +0530

    .

--- 1.)DSA/Operators_and_forLoop/fibonacciSeries.cc
+++ 1.)DSA/Operators_and_forLoop/fibonacciSeries.cc
@@ -3,8 +3,15 @@ using namespace std;
 int main(){
      int n = 10;
 
-     int a=1;
-     int b=2;
+     int a;
+     int b;
+     cout<<"Enter a Number a : ";
+     cin>>a;
+     cout<<"Enter a Number b : ";
+     cin>>b;
+
+
+     cout<<a<<" "<<b<<" ";
 
      for(int i=1; i<=n; i++){
         int nextnumber = a+b;
