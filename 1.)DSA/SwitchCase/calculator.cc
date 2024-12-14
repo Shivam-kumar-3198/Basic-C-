2eaad8298990c810f7b79dac995f3307ea0f7bcd
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-14 11:14:53 +0530

    .

---
+++ 1.)DSA/SwitchCase/calculator.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+
+    cout<<"Enter the value of a : "<<endl;
+    cin>>a;
+
+    cout<<"Enter the value of b : "<<endl;
+    cin>>b;
+
+    char op;
+    cout<<"Enter the operation (symbol) you want to perform : "<<endl;
+    cin>>op;
+
+    switch( op ){
+        cout<<"Your Result is : ";
+        case '+' : cout<<(a+b) <<endl;
+                break;
+
+        case '-' : cout<<(a-b) <<endl;
+                break;
+
+        case '*': cout<<(a*b) <<endl;
+                 break;
+
+        case '/': cout<<(a/b) <<endl;
+                 break;
+
+        case '%': cout<<(a%b) <<endl;
+                break;
+    }
+}
