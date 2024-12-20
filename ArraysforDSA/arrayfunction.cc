53b2b2d3296aeac10c32af01c620ef5883b27666
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-20 18:58:09 +0530

    .

---
+++ ArraysforDSA/arrayfunction.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size){
+    cout<<"Printing the array "<<endl;
+
+    for(int i=0; i<size; i++){
+        cout<< "Printing done" <<endl;
+    }
+}
+
+int main(){
+
+}
