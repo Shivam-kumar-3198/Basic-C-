8e0ebb024403c3da6e18c1f43eddfa06931ecbdd
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-20 11:55:31 +0530

    .

---
+++ ArraysforDSA/largestvalue.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+ void printArray(){}
+
+
+int main(){
+   // int dost[10] = {0};
+
+   // dost[0] = 1;
+  
+
+   int number[15];
+
+   cout<<"Value at 0 index : " <<number[20] <<endl;
+
+   cout<<"Everything is Fine "<<endl;
+
+   return 0;
+
+
+}
