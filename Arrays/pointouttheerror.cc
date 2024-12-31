574c6428be2c906b2ac0a05093e2c82cf0544193
Shivam <thisisshivamkumar8840@gmail.com>
2024-12-31 11:49:25 +0530

    .

---
+++ Arrays/pointouttheerror.cc
@@ -0,0 +1,15 @@
+// finding the error in this code
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for( i=1; i<=size; i++ ){  //here int i is not declared
+        cin>>arr[i];           //indexing of array will always be n-1 when it goes to 3 it will return error
+        cout<<arr[i];
+    }
+    return 0;
+}
+*/
