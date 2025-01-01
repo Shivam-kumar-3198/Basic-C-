d8bbd68ac2399e4c87a826c563909190d56b796e
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-01 19:26:34 +0530

    .

---
+++ Arrays/search.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {7,4,10,0,20,4,1,6,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x;
+    cout<<"Enter target : ";
+    cin>>x;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]);
+    }
+
+}
