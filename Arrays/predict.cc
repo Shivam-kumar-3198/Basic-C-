c684f91c74ac8905fe0f7b45a8acaa7db25b4bad
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-03 18:05:41 +0530

    .

---
+++ Arrays/predict.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int sub[50],i;
+    for(int i=0; i<=48;i++); //this semicolon make the for loop vacant
+    {
+        sub[i] = i;
+        cout<<sub[i]<<endl;
+    }
+}
