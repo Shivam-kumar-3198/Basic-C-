492d03a7d8b131f7c9bcb20a4761896255395866
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-11 23:04:44 +0530

    .

--- Vector/singlenumber.cc
+++ Vector/singlenumber.cc
@@ -1,9 +1,22 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 int main(){
-   int<vector>
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 6};  // Example input, you can modify this as needed
+    int ans = 0;
+
+    // XOR all the elements in the array
+    for(int val : nums){
+        ans = ans ^ val;
+    }
+
+    // After XOR-ing all numbers, the result will be the repeated number
+    cout << "The repeated value is " << ans << endl;
+
+    return 0;
 }
+
 /*
  XOR
  0^0 = 0;
