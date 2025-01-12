5b15830989f0f9fde45cac02c7556c865ded4e47
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-12 17:49:41 +0530

    .

--- Vector/Linearserach.cc
+++ Vector/Linearserach.cc
@@ -1,3 +1,18 @@
 /*
 We have to write linear search code for vector
 */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int search(vector<int>&arr,int x){
+    for(int i=0; i<=arr.size();i++){
+          if(arr[i] == x)
+          return i;
+          return -1;
+    }
+}
+
+int main(){
+
+}
