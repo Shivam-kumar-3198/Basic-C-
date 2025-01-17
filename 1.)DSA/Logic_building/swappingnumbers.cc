9c3b571467b2a42f95a7413f9ce3f63ec01e29c4
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-17 23:56:19 +0530

    .

---
+++ 1.)DSA/Logic_building/swappingnumbers.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =2,b=3;
+    cout<< "a = "<< a << "b = " << b << endl;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout<<"a = " << a<<"b = "<< b<< endl;
+
+    return 0;
+    
+
+}
