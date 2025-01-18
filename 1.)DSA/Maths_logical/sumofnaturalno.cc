528a2dafc688011669cb0831a2f954eacc51a0ef
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-18 23:38:16 +0530

    .

---
+++ 1.)DSA/Maths_logical/sumofnaturalno.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumOfNaturalNumbers(int n) {
+    // Using the formula n * (n + 1) / 2
+    return (n * (n + 1)) / 2;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // Output the result
+    cout << "Sum of natural numbers from 1 to " << n << " is: " 
+         << sumOfNaturalNumbers(n) << endl;
+
+    return 0;
+}
