a7c9a30382e7ad1f46a593233896970d12635c29
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-22 22:38:26 +0530

    .

--- 1.)DSA/Logic_building/swappingnumbers.cc
+++ 1.)DSA/Logic_building/swappingnumbers.cc
@@ -1,15 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a =2,b=3;
-    cout<< "a = "<< a << "b = " << b << endl;
+    // int a =2,b=3;
+    // cout<< "a = "<< a << "b = " << b << endl;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // cout<<"a = " << a<<"b = "<< b<< endl;
+
+    // return 0;
+
+    int a = 2, b = 3;
+    cout<< "a ="<<a<< "b = " << b << endl;
 
     int temp = a;
     a = b;
     b = temp;
 
-    cout<<"a = " << a<<"b = "<< b<< endl;
-
+    cout << "a =" <<a<<"b ="<< b<<endl;
     return 0;
     
 
