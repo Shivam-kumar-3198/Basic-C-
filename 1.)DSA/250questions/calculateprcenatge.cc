7d72f6f4552a80e05c468c0101847e2e4a899a63
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-27 21:50:24 +0530

    .

---
+++ 1.)DSA/250questions/calculateprcenatge.cc
@@ -0,0 +1,21 @@
+// calculate the total marks, percentage and division
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+int main(){
+   int rollno,phy,chem,it,total;
+   float percentage;
+   char name[30], div[10];
+
+   cout<<"Input the roll Number of students : ";
+   cin>>rollno;
+
+   cout<<"Input the Name of the student : ";
+   cin>>name;
+
+   cout<<"Input the marks of physics,chemistry and Information tech ..";
+   cin>>phy>>chem>>it;
+
+   total
+}
