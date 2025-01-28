bfc38b729663ee95ffaa728a886e592409e8b407
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-28 23:37:13 +0530

    .

--- 1.)DSA/250questions/eligibilityofadmission.cc
+++ 1.)DSA/250questions/eligibilityofadmission.cc
@@ -2,5 +2,13 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int candidateAge;
+    cout<<"Input the age of the candidate : ";
+    cin>>candidateAge;
 
+    if(candidateAge < 18){
+        cout<<"Sorry, you are not eligible to caste your vote : ";
+        cout<<"You would be able to caste your vote after 18 " <<18-candidateAge<<"year"<<endl;
+    
+    }
 }
