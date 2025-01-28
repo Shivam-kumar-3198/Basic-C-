241a09b50cb8feaa9cdd950a3a2b7ba36f99cd06
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-28 23:41:17 +0530

    .

--- 1.)DSA/250questions/eligibilityofadmission.cc
+++ 1.)DSA/250questions/eligibilityofadmission.cc
@@ -1,14 +1,23 @@
 // Program to find eligibility of admission
 #include<iostream>
 using namespace std;
-int main(){
-    int candidateAge;
-    cout<<"Input the age of the candidate : ";
-    cin>>candidateAge;
 
-    if(candidateAge < 18){
-        cout<<"Sorry, you are not eligible to caste your vote : ";
-        cout<<"You would be able to caste your vote after 18 " <<18-candidateAge<<"year"<<endl;
+int main() {
+    int candidateAge;
     
+    // Input the age of the candidate
+    cout << "Input the age of the candidate: ";
+    cin >> candidateAge;
+
+    // Check eligibility for voting
+    if (candidateAge < 18) {
+        cout << "Sorry, you are not eligible to cast your vote." << endl;
+        cout << "You will be able to cast your vote after " << 18 - candidateAge << " year(s)." << endl;
+    } else if (candidateAge == 18) {
+        cout << "Congratulations! You are just eligible to cast your vote." << endl;
+    } else {
+        cout << "Congratulations! You are eligible to cast your vote." << endl;
     }
+
+    return 0;
 }
