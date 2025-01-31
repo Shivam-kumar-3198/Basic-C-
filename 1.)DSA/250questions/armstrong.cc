e6becb88f21131d8db554ca1f64c64db4098cb43
Shivam <thisisshivamkumar8840@gmail.com>
2025-01-31 22:39:52 +0530

    .

--- 1.)DSA/250questions/armstrong.cc
+++ 1.)DSA/250questions/armstrong.cc
@@ -3,5 +3,15 @@
 using namespace std;
 int main()
 {
+ int lower, higher, i, temp1,temp2, remainder, n=0, result = 0;
+ cout<<"Enter two number : ";
+ cin>>lower;
+ cin>>higher;
 
+ cout<<"Armstrong number between "<<lower << "and" <<higher;
+  
+  for(i=lower; i<higher; i++){
+    temp2= i;
+    temp1= i;
+  }
 }
