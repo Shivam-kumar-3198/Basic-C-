3dfc1b88921b4190e2a54784cd6e593a877d3179
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-03 22:43:18 +0530

    .

---
+++ 1.)DSA/250questions/evenordodd.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number a: ";
+    cin >> a;
+
+    if (a % 2 == 0) {
+        cout << a << " is an Even number" << endl;
+    } else {
+        cout << a << " is an Odd number" << endl;
+    }
+
+    return 0;
+}
