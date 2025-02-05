7d92ab892507720ef783a32529ee4c1f04febbe9
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-05 22:41:17 +0530

    .

---
+++ 1.)DSA/250questions/findfactorial.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num, i;
+    long long fact =1;
+    cout<<"Enter the no to calculate factorial : ";
+    cin>>num;
+
+    fact =1;
+    i=1;
+
+    while(i<=num){
+        fact = fact * 1;
+        i++;
+    }
+    cout<<"Factorial of "<<num<<" is :"<<fact;
+
+    return 0;
+}
