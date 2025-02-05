4df19167b3e3fe39125fc9e97c3fe6df623d8cbb
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-05 22:51:14 +0530

    .

---
+++ 1.)DSA/250questions/reversealpha.cc
@@ -0,0 +1,23 @@
+// program to count number alphabets, digits and special character
+
+#include<iostream>
+#include<string.h>
+
+#define MAX_SIZE 100
+
+using namespace std;
+
+int main(){
+    char string[MAX_SIZE];
+    int aalphabets, digits, specialchars,i;
+
+    alphabets = digits = specialchars =i=0;
+
+    cout"Enter a string :";
+    cin>>string;
+
+    while(sring[i] != '/0'){
+        if((string[i] >='a' && string[i] <= 'z' \\(string[i] >=)))
+    }
+
+}
