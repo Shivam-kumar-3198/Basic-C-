fcccb0ed8c9e4e1f8b667a474de968f447204736
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-08 23:42:43 +0530

    .

---
+++ 1.)DSA/250questions/toconvertstring.cc
@@ -0,0 +1,19 @@
+// Program to convert string to lower case
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+
+    char string[10];
+    cout<<"Input a string to convert to lower case"<<endl;
+    cin.getline(string,100);
+
+    cout<<"Character in lowercase :"<<strlwr(string);
+
+    // strlwr is used to convert upper case character to lower case 
+    return 0;
+
+}
