a6e067c4789bbe8d943f737baf3d27ea5bf8aaa8
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-16 21:48:35 +0530

    .

--- 1.)DSA/250questions/armstrong.cc
+++ 1.)DSA/250questions/armstrong.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int lower, higher;
-    cout << "Enter two numbers : here : ";
+    cout << "Enter two numbers : here now : ";
     cin >> lower >> higher;
 
     cout << "Armstrong numbers between " << lower << " and " << higher << " are:\n";
