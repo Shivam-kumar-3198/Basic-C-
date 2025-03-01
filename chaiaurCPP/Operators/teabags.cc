074323a62840964327f485f770326b62a97de47b
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-01 22:35:32 +0530

    .

---
+++ chaiaurCPP/Operators/teabags.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int teaBags;
+
+    cout<<"Enter the number of tea bags you have :";
+    cin>>teaBags;
+
+    if(teaBags<10){
+           teaBags += 5;
+           
+    }
+
+    cout<<"your total bags are : "<<teaBags;
+    
+   return 0;
+}
