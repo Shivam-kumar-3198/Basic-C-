3aeebbfb3b843e59b3c98a8f97cd49cc1d12549c
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-04 22:44:56 +0530

    .

--- Arrays/7reversearray.cc
+++ Arrays/7reversearray.cc
@@ -1,11 +1,31 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[5] = {9,8,7,6,2};//no need to declare array value agar value already daal diya hai to
+//     // cout<<"Enter the numbers you want to print : ";
+//     cout<<"Registered numbers are"<<endl;
+//     for(int i=4;i>=0;i--){
+//         cout<<arr[i]<<" ";
+//     }
+
+// }
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[5] = {9,8,7,6,2};//no need to declare array value agar value already daal diya hai to
-    // cout<<"Enter the numbers you want to print : ";
-    cout<<"Registered numbers are"<<endl;
-    for(int i=4;i>=0;i--){
-        cout<<arr[i]<<" ";
+    int n;
+    cout<<"Enter the count of numbers : ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
+
+    cout<<"The numbers you entered are :";
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+        cout<<endl;
+    }
+    
 
 }
