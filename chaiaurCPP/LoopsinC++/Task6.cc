b6c4b12e7200224ad7f638c52aa427909a58668d
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-16 23:57:45 +0530

    .

--- chaiaurCPP/LoopsinC++/Task6.cc
+++ chaiaurCPP/LoopsinC++/Task6.cc
@@ -1,8 +1,18 @@
-#include<<iostream>
+#include<iostream>
 #include<string>
 
 using namespace std;
 
 int main(){
+    string teaTypes[5] = {"OOlong tea", "Orange Tea","Green Tea", "Black Tea"};
     
+
+    for(int i=0; i<5; i++){
+        cout<<"Brewing " <<teaTypes[i]<<"..." <<endl;
+
+        for(int j=1; j<3;j++){
+            cout<<"Brewing" << j<<" Cup of" <<teaTypes[i] <<endl;
+        }
+    }
+    return 0;
 }
