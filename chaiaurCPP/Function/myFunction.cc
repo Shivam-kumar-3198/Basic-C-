9f59ccc990aeb532fa6b22a52ff153a1e6644119
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-19 23:32:48 +0530

    .

--- chaiaurCPP/Function/myFunction.cc
+++ chaiaurCPP/Function/myFunction.cc
@@ -8,7 +8,12 @@ int checkTemperature(int temperature){
     // can we load data from api
 }
 
+//declaraton of function
+void serveChai(int cups);
+
 int main(){
+    int temp = checkTemperature(50);
+    std::cout<<temp;
     checkTemperature();
     return 0;
 }
