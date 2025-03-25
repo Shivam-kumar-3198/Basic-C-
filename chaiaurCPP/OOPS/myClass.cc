667a62ad60a7e2ce20c63e3bad89b8653b03ff9d
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-25 23:59:00 +0530

    .

--- chaiaurCPP/OOPS/myClass.cc
+++ chaiaurCPP/OOPS/myClass.cc
@@ -1,5 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+class Sam{
+    //data members
+    string teaName; //name of the tea
+    int servings; //Number of servings
+    vector<string> ingredients   // List of ingrediets for the tea
+};
+
 int main(){
     
+
+    return 0;
 }
