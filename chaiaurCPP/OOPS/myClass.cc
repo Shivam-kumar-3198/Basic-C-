8d1f1b24d4108370a47384500e0cbcde5c7e23af
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-26 22:22:26 +0530

    .

--- chaiaurCPP/OOPS/myClass.cc
+++ chaiaurCPP/OOPS/myClass.cc
@@ -7,6 +7,14 @@ class Sam{
     string teaName; //name of the tea
     int servings; //Number of servings
     vector<string> ingredients   // List of ingrediets for the tea
+
+    //Memeber function
+
+    void displaySam(){
+        cout<<"Tea name : " <<teaName <<endl;
+        cout<<"Servings : " <<servings <<endl;
+        
+    }
 };
 
 int main(){
