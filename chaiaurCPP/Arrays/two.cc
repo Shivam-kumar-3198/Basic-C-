af1ef54334ccc025581c10a76da33816819a2f45
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-27 23:44:28 +0530

    .

---
+++ chaiaurCPP/Arrays/two.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7] = {1,2,3,4,5,6,7};
+
+    cout<<"Numer of cups served "<<arr[0]<<endl;
+    cout<<"Numer of cups served "<<arr[1]<<endl;
+    cout<<"Numer of cups served "<<arr[2]<<endl;
+    cout<<"Numer of cups served "<<arr[3]<<endl;
+    cout<<"Numer of cups served "<<arr[4]<<endl;
+    cout<<"Numer of cups served "<<arr[5]<<endl;
+    cout<<"Numer of cups served "<<arr[6]<<endl;
+
+}
