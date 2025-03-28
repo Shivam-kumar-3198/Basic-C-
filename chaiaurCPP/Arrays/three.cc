e85e2cc661c3ce9c14adc0e65b8b2d2d367ccaec
Shivam <thisisshivamkumar8840@gmail.com>
2025-03-28 23:30:03 +0530

    .

--- chaiaurCPP/Arrays/three.cc
+++ chaiaurCPP/Arrays/three.cc
@@ -1,14 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int* prepare ChaiOrders(int cups){
+int* prepareChaiOrders(int cups) {
     int* orders = new int[cups];
-    for(int i=0; i<cups; i++){
-        orders[i] = (i + 1) * 10;
+    for (int i = 0; i < cups; i++) {
+        orders[i] = (i + 1) * 10; // Each cup has increasing amounts of chai
     }
     return orders;
 }
 
-int main(){
+int main() {
+    int cups = 5;
+    int* chaiOrder = prepareChaiOrders(cups);
 
+    for (int i = 0; i < cups; i++) {
+        cout << "Cup " << (i + 1) << " has " << chaiOrder[i] << " ml" << endl;
+    }
+
+    // Free dynamically allocated memory
+    delete[] chaiOrder;
+
+    return 0;
 }
