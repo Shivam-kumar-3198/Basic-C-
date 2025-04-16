eb46d1405708dbd01676c882d978cf35f5a34063
Shivam <thisisshivamkumar8840@gmail.com>
2025-04-16 23:37:24 +0530

    .

---
+++ GoogleInterviewQuestions/FarmerProblem.cc
@@ -0,0 +1,18 @@
+/*
+A farmer wants to
+farm their land with the maximum area where good land is present.
+The
+"Land"
+is represented as a matrix with ls and 0s,
+where is mean good land and 0s
+mean bad land. The farmer only want to farm in a square of good land with the maximum
+area. Please help the farmer to find the maximum area of the land they can farm in
+good land.
+Example:
+01101
+1 1 0 1 1
+0 1 1 1 0
+1 1 1 1 0
+1 1 1 1 1
+0 0 0 0 0
+*/
