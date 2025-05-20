6c6a98da515d5ebf99876ec7f9427ac223ac7aff
Shivam <thisisshivamkumar8840@gmail.com>
2025-05-20 06:36:07 +0530

    .

---
+++ Company150questions/armstrng.cc
@@ -0,0 +1,19 @@
+/*
+Calculating Armstrong Numbers
+Difficulty: Easy
+Topics: Basic Programming, Number Theory
+Description: Write a program to check if a number is an Armstrong number.
+Example:
+Input: number = 153
+Output: Armstrong Number
+Explanation: 153 is an Armstrong number because 1^3 + 5^3 + 3^3 = 153.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    
+}
