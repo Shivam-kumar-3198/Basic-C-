bca51eb2352e2cf0c2a47b75bc344735ea0671ef
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-08 19:41:51 +0530

    .

--- 150_questions_sprints/fibonacci.cc
+++ 150_questions_sprints/fibonacci.cc
@@ -8,21 +8,40 @@ Input: limit = 10
 Output: [0, 1, 1, 2, 3, 5, 8]
 Explanation: The Fibonacci series up to 10 is generated as [0, 1, 1, 2, 3, 5, 8].
 */
- #include<iostream>
- using namespace std;
+//  #include<iostream>
+//  using namespace std;
 
- int main(){
+//  int main(){
+//     int n;
+//     cout<<"Enter a number n to print fibonacci sreies : "<<endl;
+//     cin>>n;
+
+//     int a=0, b=1;
+
+//     for(int i=0;i<n;i++){
+//         cout<<a<<" ";
+//         int next = a+b;
+//         a=b;
+//         b=next;
+//     }
+//     return 0;
+//  }
+
+#include<iostream>
+using namespace std;
+
+int main(){
     int n;
-    cout<<"Enter a number n to print fibonacci sreies : "<<endl;
+    cout<<"Enter the term you want to print number of times the fiboonaci series : "<<endl;
     cin>>n;
 
     int a=0, b=1;
 
-    for(int i=0;i<n;i++){
-        cout<<a<<" ";
+    for(int i=0; i<=n; i++){
+        cout<<a << " ";  
         int next = a+b;
         a=b;
         b=next;
     }
     return 0;
- }
+}
