a79253390ef998b39e9fb4195727f2b615fe95e4
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-09 11:53:10 +0530

    .

--- 150_questions_sprints/fibonacci.cc
+++ 150_questions_sprints/fibonacci.cc
@@ -27,21 +27,23 @@ Explanation: The Fibonacci series up to 10 is generated as [0, 1, 1, 2, 3, 5, 8]
 //     return 0;
 //  }
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the term you want to print number of times the fiboonaci series : "<<endl;
-    cin>>n;
+    cout << "Enter the term you want to print number of times the fiboonaci series : " << endl;
+    cin >> n;
 
-    int a=0, b=1;
+    int a = 0, b = 1;
 
-    for(int i=0; i<=n; i++){
-        cout<<a << " ";  
-        int next = a+b;
-        a=b;
-        b=next;
+    for (int i = 0; i <= n; i++)
+    {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
     }
     return 0;
 }
