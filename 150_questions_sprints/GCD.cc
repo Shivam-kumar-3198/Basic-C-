f5e7bde47cf9fe8f0bbaccda651e49c876802947
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-17 23:05:19 +0530

    .

---
+++ 150_questions_sprints/GCD.cc
@@ -0,0 +1,17 @@
+/*
+Finding the Greatest Common Divisor (GCD)
+Difficulty: Easy
+Topics: Basic Programming, Number Theory
+Description: Write a program to find the GCD of two numbers.
+Example:
+Input: a = 48, b = 18
+Output: 6
+Explanation: The GCD of 48 and 18 is 6.
+*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
