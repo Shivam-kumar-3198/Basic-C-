889cf9c0d54ce9eeb9435466f91b3471a86b7ede
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-18 22:39:57 +0530

    .

--- 150_questions_sprints/GCD.cc
+++ 150_questions_sprints/GCD.cc
@@ -12,6 +12,21 @@ Explanation: The GCD of 48 and 18 is 6.
 
 #include<iostream>
 using namespace std;
+
+int gcd(int a, int b){
+    while(b!=0){
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+}
 int main(){
+    int a,b;
     
+    
+    cin>>a>>b;
+
+    cout<<gcd(a,b)<<endl;
+
 }
