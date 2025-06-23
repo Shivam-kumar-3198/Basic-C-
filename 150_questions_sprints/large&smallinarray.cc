0b1dafb48bca8150983444c300e4a242cfdb6b82
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-23 19:52:15 +0530

    .

--- 150_questions_sprints/large&smallinarray.cc
+++ 150_questions_sprints/large&smallinarray.cc
@@ -12,15 +12,15 @@ Explanation: The largest number in the array is 8 and the smallest is 1.
 using namespace std;
 int main()
 {
-    int arr[] = {4, 7, 1, 8, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {4, 7, 1, 8, 5};  //we have stored an array here
+    int n = sizeof(arr) / sizeof(arr[0]); //Now we have to know the length of array how can  we do that 
 
-    int largest = arr[0];
-    int smallest = arr[0];
+    int largest = arr[0]; //here the index position of array is stored
+    int smallest = arr[0]; //also smallest is considered as index position
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++) //traverse the array and move forward
     {
-        if (arr[i] > largest)
+        if (arr[i] > largest)  //if 
         {
             largest = arr[i];
         }
