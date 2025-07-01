e4fe6235999341c361e9efdc20904ec4d4bd14b8
Shivam <thisisshivamkumar8840@gmail.com>
2025-07-01 23:11:24 +0530

    .

--- 150_questions_sprints/ArmstrongRange.cc
+++ 150_questions_sprints/ArmstrongRange.cc
@@ -8,8 +8,48 @@ Input: range = [1, 500]
 Output: [1, 153, 370, 371, 407]
 Explanation: Armstrong numbers between 1 and 500 are 1, 153, 370, 371, and 407.
 */
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
+
+// Function to check if a number is Armstrong
+bool isArmstrong(int num) {
+    int originalNum = num;
+    int sum = 0;
+    int digits = 0;
     
+    // Count digits
+    int temp = num;
+    while (temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    // Calculate sum of digits raised to the power of number of digits
+    temp = num;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    return sum == originalNum;
+}
+
+int main() {
+    int lower, upper;
+    
+    cout << "Enter lower bound of the range: ";
+    cin >> lower;
+    cout << "Enter upper bound of the range: ";
+    cin >> upper;
+
+    cout << "Armstrong numbers between " << lower << " and " << upper << " are:\n";
+    for (int i = lower; i <= upper; i++) {
+        if (isArmstrong(i)) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
 }
