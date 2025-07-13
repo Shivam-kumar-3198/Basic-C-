578b40cecac2e53972039c5d30195506361f6d1a
Shivam <thisisshivamkumar8840@gmail.com>
2025-07-13 22:18:01 +0530

    .

---
+++ Recursion/L7.cc
@@ -0,0 +1,26 @@
+//Reverse a nam using recursion
+#include<iostream>
+using namespace std;
+
+void reverse(string& str, int i, int j){
+    if(i>j)
+    return ;
+
+
+    swap(str[i], str[j]);
+    i++;
+    j--;
+
+    reverse(str,i,j);
+}
+
+int main(){
+    string name="Shivam";
+
+    reverse(name, 0, name.length()-1);
+
+    cout<< name <<endl;
+
+    return 0;
+
+}
