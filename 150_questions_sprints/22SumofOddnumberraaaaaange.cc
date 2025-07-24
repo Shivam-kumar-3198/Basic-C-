7622307955fcb0fb978069a4314f9238b43f4954
Shivam <thisisshivamkumar8840@gmail.com>
2025-07-24 07:28:59 +0530

    .

---
+++ 150_questions_sprints/22SumofOddnumberraaaaaange.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int start, end;
+    cout << "Enter the starting range of number : ";
+    cin >> start;
+    cout << "Enter the ending range of number : ";
+    cin >> end;
+
+    // Adjust to the first and last odd numbers
+    if (start % 2 == 0) start++;
+    if (end % 2 == 0) end--;
+
+    // If no odd numbers in range
+    if (start > end) {
+        cout << "No odd numbers in the given range." << endl;
+        return 0;
+    }
+
+    int n = ((end - start) / 2) + 1;
+    int sum = n * (start + end) / 2;
+
+    cout << "Sum of odd numbers between " << start << " and " << end << " is: " << sum << endl;
+
+    return 0;
+}
