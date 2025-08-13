b3df530d4f186799c1d50204fed4ab7a38aed6a7
Shivam <thisisshivamkumar8840@gmail.com>
2025-08-13 18:05:33 +0530

    .

---
+++ 150_questions_sprints/35.PrimeNumberRange.cc
@@ -0,0 +1,27 @@
+/*
+Finding the Sum of Prime Numbers in a Range
+Difficulty: Medium
+Topics: Number Theory, Mathematical Computations
+Description: Write a program to calculate the sum of all prime numbers within a given range.
+Example:
+Input: range = [1, 10]
+Output: 17
+Explanation: The sum of prime numbers between 1 and 10 is 2 + 3 + 5 + 7 = 17.
+*/
+#include<iostream>
+using namespace std;
+
+void checkPrimeNumber(){
+    if(n%1==0 && n%n==0)
+}
+
+void primeNumberSum(){
+    
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number to check is it prime or not : ";
+    cin>>n;
+
+}
