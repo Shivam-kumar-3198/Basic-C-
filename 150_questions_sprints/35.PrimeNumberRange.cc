bf33bf17c7280cfa0d425b123694bb1bbfa2abc2
Shivam <thisisshivamkumar8840@gmail.com>
2025-08-14 17:31:21 +0530

    .

--- 150_questions_sprints/35.PrimeNumberRange.cc
+++ 150_questions_sprints/35.PrimeNumberRange.cc
@@ -9,19 +9,39 @@ Output: 17
 Explanation: The sum of prime numbers between 1 and 10 is 2 + 3 + 5 + 7 = 17.
 */
 #include<iostream>
+#include<cmath>
 using namespace std;
 
-void checkPrimeNumber(){
-    if(n%1==0 && n%n==0)
+bool checkPrimeNumber(int n){
+    if(n<2) return false;
+
+    for(int i=2; i<= sqrt(n); i++){
+        if(n%i==0) return false;
+       
+    }
+    return true;
 }
 
-void primeNumberSum(){
+int primeNumberSum(int start, int end){
+    int sum=0;
     
+    for(int i= start; i<= end; i++){
+        if(checkPrimeNumber(i)){
+            sum += i;
+        }
+    }
+    return sum;
 }
 
-int main(){
-    int n;
-    cout<<"Enter the number to check is it prime or not : ";
-    cin>>n;
+int main() {
+    int start, end;
+    cout << "Enter start of range: ";
+    cin >> start;
+    cout << "Enter end of range: ";
+    cin >> end;
 
+    int sum = primeNumberSum(start, end);
+    cout << "Sum of prime numbers between " << start << " and " << end << " = " << sum << endl;
+
+    return 0;
 }
