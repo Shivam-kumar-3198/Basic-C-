0a270afb9b8707e6d76f15d39f600f7e43c3b428
Shivam <thisisshivamkumar8840@gmail.com>
2025-08-21 17:04:26 +0530

    .

--- 150_questions_sprints/44FindingAverage.cc
+++ 150_questions_sprints/44FindingAverage.cc
@@ -7,6 +7,28 @@ Example:
 Input: array = [1, 2, 3, 4, 5]
 Output: 3
 Explanation: The average of the numbers is (1 + 2 + 3 + 4 + 5) / 5 = 3.
-
-
 */
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the count of numbers you want to make average : ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the numbers : ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum+= arr[i];
+    }
+
+    double average = (double)sum/n;
+    cout<<"Average = "<<average<<endl;
+    return 0;
+
+    
+}
