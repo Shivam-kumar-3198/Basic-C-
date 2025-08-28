aa630099d226521219fd4e9f20ffbac84f6dcc13
Shivam <thisisshivamkumar8840@gmail.com>
2025-08-28 17:21:06 +0530

    .

--- 150_questions_sprints/48Findingsumogprime.cc
+++ 150_questions_sprints/48Findingsumogprime.cc
@@ -12,6 +12,39 @@ Explanation: The prime factors of 12 are 2 and 3, and their sum is 2 + 3 = 5.
 #include<iostream>
 using namespace std;
 
-int main(){
+bool isPrime(int n){
+    if(n <= 1) return false;
+
+    for(int i=2; i*i<=n; i++){
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+
+int sumofPrimeFactors(int n){
+    int sum = 0;
+
+    for(int i=2; i<=n; i++){
+        if(n%i==0 && isPrime(i)){
+            sum += i;
+
+            while(n%i==0){
+                n /= i;
+            }
+        }
+    }
+return sum;
     
+}  
+
+int main(){
+    int number;
+    cout<<" Enter a number : ";
+    cin>>number;
+
+    int result = sumofPrimeFactors(number);
+
+    cout<<"Sum of prime factors : "<<result<<endl;
+
+    return 0;
 }
