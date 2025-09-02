83adaf95d7b76c506ab0b0fffd19da47a76fb395
Shivam <thisisshivamkumar8840@gmail.com>
2025-09-02 18:38:57 +0530

    .

--- 150_questions_sprints/53calculatingsumofseries.cc
+++ 150_questions_sprints/53calculatingsumofseries.cc
@@ -11,3 +11,19 @@ Explanation: Sum of the series is 1 + 1/2 + 1/3 + 1/4 ≈ 2.083333.
 
 #include<iostream>
 using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+
+    double sum = 0.0;
+
+    for(int i=1; i<=n; i++){
+        sum += 1.0/i;
+    }
+
+    cout<<"Sum of the series = " <<fixed <<sum<<endl;
+
+    return 0;
+}
