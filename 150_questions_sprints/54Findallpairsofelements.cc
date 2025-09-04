6f8f15ab261a6be8743d49f388bcb0885274e44d
Shivam <thisisshivamkumar8840@gmail.com>
2025-09-04 17:26:27 +0530

    .

--- 150_questions_sprints/54Findallpairsofelements.cc
+++ 150_questions_sprints/54Findallpairsofelements.cc
@@ -9,8 +9,32 @@ Output: [(1, 4), (2, 3)]
 Explanation: Pairs that sum up to 5 are (1, 4) and (2, 3).
 */
 #include<iostream>
+#include<vector>
 using namespace std;
 
+vector<pair<int, int>> findPairsBruteForce(vector<int> & arr, int target){
+    vector<pair<int, int>> pairs;
+    int n= arr.size();
+
+
+for(int i=0; i<n; i++){
+    for(int j=i+1; j<n; j++){
+        if(arr[i] + arr[j] == target){
+            pairs.push_back({arr[i], arr[j]});
+        }
+    }
+}
+return pairs;
+}
+
 int main(){
-    
+    vector<int> arr={1,2,3,4,5};
+    int target=5;
+
+    vector<pair<int, int>> result = findPairsBruteForce(arr, target);
+
+    for(auto &p : result){
+        cout<<"(" << p.first << ", " << p.second << ") ";
+    }
+    return 0;
 }
